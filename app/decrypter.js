aa3073b141e68c30a9fc09d8c3d3bf5f86cb18fa
Rellyson Silva <rellysonsilva@gmail.com>
2020-03-21 16:46:29 -0300

    comments added to decrypter.js

--- app/decrypter.js
+++ app/decrypter.js
@@ -1,21 +1,29 @@
-let answer = require('./answer.json');
+const sha1 = require('sha1');
+const answer = require('./answer.json');
 
 let encrypted_mesage = answer.cifrado;
-let decrypted_message = answer.decifrado;
+let numero_casas = answer.numero_casas;
+let decrypted_message;
 
+//loop to get every letter in the string given
 for (let letter in encrypted_mesage) {
 
+    //now we convert the letter in index on a Unicode
     let decrypting_letter = encrypted_mesage.charCodeAt(letter);
 
+    //checking if our generated Unicode is between 97 and 122, that corresponds to the alplhabet
     if (decrypting_letter >= 97 && decrypting_letter <=122 ){
-        decrypting_letter = decrypting_letter - answer.numero_casas;
 
-        if(decrypting_letter < 97)
+        //now we subtract the Unicode by the numero_casas given 
+        decrypting_letter = decrypting_letter - numero_casas;
+
+        //if after the subtract, the Unicode is less than 97, we add 26 to have the correct Unicode position 
+        if (decrypting_letter < 97)
             decrypting_letter = decrypting_letter + 26;
     }
 
-
-    let decrypted_letter = String.fromCharCode(decrypting_letter);
+    // generating a new string with our decrypted message
+    decrypted_message += String.fromCharCode(decrypting_letter);
 }
 
 
