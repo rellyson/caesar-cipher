35b3b3160cea24b6d55c418ea58df3400734d139
Rellyson Silva <rellysonsilva@gmail.com>
2020-03-21 20:16:26 -0300

    just need to solve form appends

--- app/decrypter.js
+++ app/decrypter.js
@@ -1,6 +1,8 @@
 const sha1 = require('sha1');
 const answer = require('./answer.json');
 const jsonUpdate = require('json-update');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
 let encrypted_mesage = answer.cifrado;
 let numero_casas = answer.numero_casas;
@@ -33,13 +35,22 @@ let resumo_criptografico = sha1(decrypted_message);
 //updating json at decifrado and resumo_criptografico fields
 jsonUpdate.update('./app/answer.json', { decifrado: decrypted_message, resumo_criptografico: resumo_criptografico })
     .then(() => {
-        console.log("json file updated!");
-    })
-    .then(() => {
-        //logs the answer.json file updated
-        console.log(answer);
-    });
+        console.log("done!");
+    }
+);
+
+//crating new form
+const form = new FormData();
+
+//append every field from json
+for (let item in answer) {
+
+    form.append('answer',item,answer[item]);
+}
 
 
-
-//https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=425dea77c911d0eb76a0384e7b5989b912b3a9e4
+//http post to server passing Form.data 
+fetch('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=425dea77c911d0eb76a0384e7b5989b912b3a9e4',
+     {method: 'POST', body: form})
+	.then(res => res.json())
+	.then(json => console.log(json));
